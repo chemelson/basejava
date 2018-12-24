00ab5cf89b96905132ff6b5bbf765dcea6c40b62
Anton Chemlev <ross03@mail.ru>
2018-12-24 22:42:29 +0300

    HW3: Fixed (i hope so) arrat out of bound error.

--- src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
@@ -22,8 +22,8 @@ public class SortedArrayStorage extends AbstractArrayStorage {
 
     protected void remove(int index) {
         int lastIndex = size - 1;
-        if ((size != 1) || (index != lastIndex)) {
-            System.arraycopy(storage, index + 1, storage, index, size - 1);
+        if ((size != 1) && (index != lastIndex)) {
+            System.arraycopy(storage, index + 1, storage, index, size - (index + 1));
         }
         storage[lastIndex] = null;
     }
