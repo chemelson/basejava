ee085efb522bc3ded2a8bb6e5251f2dd0b4714a6
Anton Chemlev <ross03@mail.ru>
2019-01-10 21:35:01 +0300

    HW4: AbstractArrayStorageTest ready for check

--- test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
+++ test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
@@ -28,32 +28,42 @@ public abstract class AbstractArrayStorageTest {
 
     @Test
     public void clear() throws Exception {
-
+        storage.clear();
+        Assert.assertEquals(0, storage.size());
     }
 
     @Test
     public void update() throws Exception {
-
+        Resume old_resume = storage.get(UUID_1);
+        // TODO: update Resume object
+        storage.update(old_resume);
+        Resume updated_resume = storage.get(UUID_1);
+        Assert.assertEquals(old_resume, updated_resume);
     }
 
     @Test
     public void getAll() throws Exception {
-
+        Resume[] resumes = {new Resume(UUID_1), new Resume(UUID_2), new Resume(UUID_3)};
+        Assert.assertArrayEquals(resumes, storage.getAll());
     }
 
     @Test
     public void save() throws Exception {
-
+        Resume resume = new Resume("uuid4");
+        storage.save(resume);
+        Assert.assertEquals(4, storage.size());
     }
 
     @Test
     public void delete() throws Exception {
-
+        storage.delete(UUID_1);
+        Assert.assertEquals(2, storage.size());
     }
 
     @Test
     public void get() throws Exception {
-
+        Resume resume = storage.get(UUID_1);
+        Assert.assertEquals(UUID_1, resume.getUuid());
     }
 
     @Test(expected = NotExistStorageException.class)
