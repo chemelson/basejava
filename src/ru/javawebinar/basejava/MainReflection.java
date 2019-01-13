27036ea4ee35bd3a80cebf3f04b0a4d6c4f43f4a
Anton Chemlev <ross03@mail.ru>
2019-01-13 11:21:47 +0300

    HW4: Ready for review

--- src/ru/javawebinar/basejava/MainReflection.java
+++ src/ru/javawebinar/basejava/MainReflection.java
@@ -3,17 +3,21 @@ package ru.javawebinar.basejava;
 import ru.javawebinar.basejava.model.Resume;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class MainReflection {
 
-    public static void main(String[] args) throws IllegalAccessException {
+    public static void main(String[] args) throws IllegalAccessException,
+            NoSuchMethodException, InvocationTargetException {
         Resume r = new Resume();
         Field field = r.getClass().getDeclaredFields()[0];
         field.setAccessible(true);
         System.out.println(field.getName());
         System.out.println(field.get(r));
         field.set(r, "new_uuid");
-
-        System.out.println(r);
+        System.out.println("\n----------");
+        Method method = r.getClass().getDeclaredMethod("toString");
+        System.out.println(method.invoke(r));
     }
 }
