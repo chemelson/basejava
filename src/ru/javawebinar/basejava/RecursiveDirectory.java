0e9b789321da98490d6726e3579bc834739b885d
achemlev <achemlev@stc.team>
2019-03-04 16:16:15 +0300

    HW8: Display only files

--- src/ru/javawebinar/basejava/RecursiveDirectory.java
+++ src/ru/javawebinar/basejava/RecursiveDirectory.java
@@ -1,27 +1,19 @@
 package ru.javawebinar.basejava;
 
 import java.io.File;
-import java.io.IOException;
 
 public class RecursiveDirectory {
     public static void main(String[] args) {
-        try {
             File root = new File(".");
-            printDirectory(root, 0);
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
+            printDirectory(root);
     }
 
-    private static void printDirectory(File root, int tabLevel) throws IOException {
-        StringBuilder tabs = new StringBuilder();
-        for (int i = 0; i < tabLevel; i++) {
-            tabs.append("\t");
-        }
-        System.out.println(tabs.toString() + root.getName());
-        if (root.isDirectory()) {
+    private static void printDirectory(File root) {
+       if (!root.isDirectory()) {
+            System.out.println(root.getName());
+        } else {
             for (File item : root.listFiles()) {
-                printDirectory(item, tabLevel + 1);
+                printDirectory(item);
             }
         }
     }
