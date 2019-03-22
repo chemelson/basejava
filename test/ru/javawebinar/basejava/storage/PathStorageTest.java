210c428517a0facdb83f8f5d0d2008ffc440e356
chemelson <ross03@mail.ru>
2019-03-22 16:27:31 +0300

    HW9: Added FileStorage, PathStorage and tests for them.

--- test/ru/javawebinar/basejava/storage/PathStorageTest.java
+++ test/ru/javawebinar/basejava/storage/PathStorageTest.java
@@ -3,6 +3,6 @@ package ru.javawebinar.basejava.storage;
 public class PathStorageTest extends AbstractStorageTest {
 
     public PathStorageTest() {
-        super(new PathStorage(STORAGE_PATH_DIR, new ObjectStreamSerializationStrategy()));
+        super(new FileStorage(STORAGE_FILE_DIR, new ObjectStreamSerializationStrategy()));
     }
 }
