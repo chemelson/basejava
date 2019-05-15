ca7ffbeb4f070c2fd08cf4832da65f7f6eb16f35
Anton Chemlev <ross03@mail.ru>
2019-05-15 09:33:17 +0300

    HW16: JSP commit

--- src/ru/javawebinar/basejava/web/ResumeServlet.java
+++ src/ru/javawebinar/basejava/web/ResumeServlet.java
@@ -28,32 +28,9 @@ public class ResumeServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setCharacterEncoding("UTF-8");
-        response.setCharacterEncoding("UTF-8");
-        response.setContentType("text/html; charset=UTF-8");
-        List<Resume> resumes = storage.getAllSorted();
-        String htmlPage = getHtmlPage(resumes);
-        response.getWriter().write(htmlPage);
+        request.setAttribute("resumes", storage.getAllSorted());
+        request.getRequestDispatcher("/WEB-INF/jsp/list.jsp").forward(request, response);
     }
 
-    private String getHtmlPage(List<Resume> resumes) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("<html><head><title>All resumes</title></head><body>");
-        if (resumes.size() > 0) {
-            sb.append("<table>");
-            sb.append("<tr><th>UUID</th><th>Fullname</th></tr>");
-            resumes.forEach(resume -> {
-                sb.append("<tr><td>");
-                sb.append(resume.getUuid());
-                sb.append("</td><td>");
-                sb.append(resume.getFullName());
-                sb.append("</td></td>");
-            });
-            sb.append("</table>");
-        } else {
-            sb.append("<p>No resumes</p>");
-        }
-        sb.append("</body></html>");
-        return sb.toString();
-    }
+
 }
